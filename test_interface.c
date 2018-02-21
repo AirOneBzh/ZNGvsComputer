6bc6aa856906b2e365b89c501de7b48b2b62be53
root <root@localhost.localdomain>
2018-02-21 18:04:49 +0100

    2

--- test_interface.c
+++ test_interface.c
@@ -11,7 +11,7 @@ int main() {
   MLV_Music* music=MLV_load_music("assets/sounds/gameover.ogg");
   MLV_Image *image;
   int image_width, image_height;//, text_width, text_height;
-  const char* text="Ordinateur Gagne! \n Esc pour retour menu.";
+  const char* text="Ordinateur Gagne! Esc pour retour menu.";
 
   MLV_create_window("ZNG vs Computer", "game", width, height);
   image=MLV_load_image("assets/images/gameover.jpg");
@@ -24,12 +24,11 @@ int main() {
 
   MLV_play_music(music, 1.0, -1);
   MLV_actualise_window();
-  if(MLV_get_event(&esc, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)==MLV_KEYBOARD_ESCAPE){//MLV_KEYBOARD_ESCAPE = SDLK_ESCAPE
-  MLV_free_image(image);
-  MLV_stop_music();
-  MLV_free_music(music);
-  MLV_free_audio();
-  MLV_free_window();
-  return 0;
-}
+  MLV_wait_event(&esc, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);//MLV_KEYBOARD_ESCAPE = SDLK_ESCAPE
+    MLV_free_image(image);
+    MLV_stop_music();
+    MLV_free_music(music);
+    MLV_free_audio();
+    MLV_free_window();
+    return 0;
 }
