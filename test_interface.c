b8ed87fe7847d69f305f4d557f6e8b13eaadb0fd
root <root@localhost.localdomain>
2018-02-21 17:28:56 +0100

    not empty

--- test_interface.c
+++ test_interface.c
@@ -10,13 +10,13 @@ int main() {
   int image_width, image_height;//, text_width, text_height;
   const char* text="Ordinateur Gagne! \n Esc pour retour menu.";
 
-  MLV_create_window( "Player vs Game", "game", width, height);
-  image = MLV_load_image( "assets/images/gameover.jpg" );
+  MLV_create_window("ZNG vs Computer", "game", width, height);
+  image=MLV_load_image("assets/images/gameover.jpg");
   MLV_resize_image_with_proportions(image, width, height);
   MLV_get_image_size(image, &image_width, &image_height);
   MLV_draw_image( image, 0, 0 );
   MLV_get_size_of_text(text, &image_width, &image_height);
-  MLV_draw_text(width/3.5, height-90, text, MLV_COLOR_GREEN );
+  MLV_draw_text(width/3.5, height-90, text, MLV_COLOR_GREEN);
 
 
   MLV_actualise_window();
