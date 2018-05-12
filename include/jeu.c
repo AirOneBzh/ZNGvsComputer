badc2851d29eb5e2e15f025ba540a2c1ad3630bb
Césarus <cami_b69@hotmail.fr>
2018-05-12 20:24:57 +0200

    cap_haut return 0

--- include/jeu.c
+++ include/jeu.c
@@ -134,13 +134,13 @@ int cap_haut(int couleur, int i, int j, int **plateau){
 	if( plateau[i_bis][j] == couleur && adv > 0 )
 	    return 1;
 	if( plateau[i_bis][j] == VIDE)
-	    return 1;
+	    return 0;
 	else{
 	    adv+=1;
 	    i_bis-=1;
 	}
     }
-    return 1;
+    return 0;
 }
 
 int cap_bas(int couleur, int i, int j, int **plateau){
